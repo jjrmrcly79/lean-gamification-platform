87075e8fe350cdfc1e0fc5be14f4095260753d91
jjrmrcly79 <juanjose.garces@gmail.com>
2025-09-19 01:06:58 -0600

    ajustesfinales

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+// Importamos el tipo 'Configuration' directamente desde webpack
+import type { Configuration } from 'webpack';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Le indicamos a la función que el parámetro 'config' es de tipo 'Configuration'
+  webpack: (config: Configuration) => {
+    // Es buena práctica verificar si module y rules existen antes de usarlos
+    if (config.module?.rules) {
+      config.module.rules.push({
+        test: /pdf\.worker\.min\.js$/,
+        type: "asset/resource",
+        generator: {
+          filename: "static/chunks/[name].[hash][ext]",
+        },
+      });
+    }
+
+    return config;
+  },
 };
 
 export default nextConfig;
