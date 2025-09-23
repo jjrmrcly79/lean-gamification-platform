d6c8b8a741527fb5a3444d7efb61f0a84a1fd18b
jjrmrcly79 <juanjose.garces@gmail.com>
2025-09-23 13:11:19 -0600

    ajustefinal3

--- types/supabase.ts
+++ types/supabase.ts
@@ -1,3 +1,19 @@
+[?25l[?2004h
+                                                                                                       
+  >  1. dicshuqncgxkpvitksco [name: jjrmrcly79's Project, org: jlrvqhjdgfxjdgyfrzva, region: us-east-1]
+    2. tilxwopmazrtwqzosaqm [name: tiendas_ecomerce, org: jlrvqhjdgfxjdgyfrzva, region: us-east-2]     
+                                                                                                       
+                                                                                                       
+                                                                                                       
+                                                                                                       
+    ↑/k up • ↓/j down • / filter • q quit • ? more                                                     
+                                                                                                       [D[9A [J[D[2K[?2004l[?25h[?1002l[?1003l[?1006lexport type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
 
 export type Database = {
   // Allows to automatically instantiate createClient with right options
@@ -17,9 +33,9 @@ export type Database = {
           involucramiento_score: number | null
           kaizen_score: number | null
           perfil_score: number | null
-          questions_data: JSON | null
-          score_by_category: JSON | null
-          score_by_subcategory: JSON | null
+          questions_data: Json | null
+          score_by_category: Json | null
+          score_by_subcategory: Json | null
           sostenimiento_score: number | null
           status: string
           user_id: string | null
@@ -33,9 +49,9 @@ export type Database = {
           involucramiento_score?: number | null
           kaizen_score?: number | null
           perfil_score?: number | null
-          questions_data?: JSON | null
-          score_by_category?: JSON | null
-          score_by_subcategory?: JSON | null
+          questions_data?: Json | null
+          score_by_category?: Json | null
+          score_by_subcategory?: Json | null
           sostenimiento_score?: number | null
           status?: string
           user_id?: string | null
@@ -49,9 +65,9 @@ export type Database = {
           involucramiento_score?: number | null
           kaizen_score?: number | null
           perfil_score?: number | null
-          questions_data?: JSON | null
-          score_by_category?: JSON | null
-          score_by_subcategory?: JSON | null
+          questions_data?: Json | null
+          score_by_category?: Json | null
+          score_by_subcategory?: Json | null
           sostenimiento_score?: number | null
           status?: string
           user_id?: string | null
@@ -71,7 +87,7 @@ export type Database = {
           created_at: string
           id: number
           operation_name: string
-          result_topics: JSON | null
+          result_topics: Json | null
           source_document_name: string | null
           status: string
           user_id: string | null
@@ -80,7 +96,7 @@ export type Database = {
           created_at?: string
           id?: number
           operation_name: string
-          result_topics?: JSON | null
+          result_topics?: Json | null
           source_document_name?: string | null
           status?: string
           user_id?: string | null
@@ -89,7 +105,7 @@ export type Database = {
           created_at?: string
           id?: number
           operation_name?: string
-          result_topics?: JSON | null
+          result_topics?: Json | null
           source_document_name?: string | null
           status?: string
           user_id?: string | null
@@ -100,21 +116,21 @@ export type Database = {
         Row: {
           created_at: string
           description: string | null
-          example: JSON | null
+          example: Json | null
           id: string
           name: string | null
         }
         Insert: {
           created_at?: string
           description?: string | null
-          example?: JSON | null
+          example?: Json | null
           id: string
           name?: string | null
         }
         Update: {
           created_at?: string
           description?: string | null
-          example?: JSON | null
+          example?: Json | null
           id?: string
           name?: string | null
         }
@@ -126,7 +142,7 @@ export type Database = {
           created_at: string | null
           id: string
           prompt: string | null
-          question_data: JSON | null
+          question_data: Json | null
           subcategory: string | null
           type: string | null
         }
@@ -135,7 +151,7 @@ export type Database = {
           created_at?: string | null
           id: string
           prompt?: string | null
-          question_data?: JSON | null
+          question_data?: Json | null
           subcategory?: string | null
           type?: string | null
         }
@@ -144,7 +160,7 @@ export type Database = {
           created_at?: string | null
           id?: string
           prompt?: string | null
-          question_data?: JSON | null
+          question_data?: Json | null
           subcategory?: string | null
           type?: string | null
         }
@@ -176,21 +192,21 @@ export type Database = {
           created_at: string
           documento_origen: string | null
           id: string
-          temas: JSON | null
+          temas: Json | null
           user_id: string
         }
         Insert: {
           created_at?: string
           documento_origen?: string | null
           id?: string
-          temas?: JSON | null
+          temas?: Json | null
           user_id?: string
         }
         Update: {
           created_at?: string
           documento_origen?: string | null
           id?: string
-          temas?: JSON | null
+          temas?: Json | null
           user_id?: string
         }
         Relationships: [
