73061744592fe13a0258e9e4e535855caea02f07
jjrmrcly79 <juanjose.garces@gmail.com>
2025-10-07 14:46:27 -0600

    matriz14

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,16 +1,15 @@
 // postcss.config.mjs
 export default {
   plugins: {
-    tailwindcss: {},
-    // Convierte lab()/lch()/oklab()/oklch() a rgb() en el build
-    'postcss-preset-env': {
+    "@tailwindcss/postcss": {},
+    "postcss-preset-env": {
       stage: 1,
       features: {
-        'lab-function': true,
-        'lch-function': true,
-        'oklab-function': true,
-        'oklch-function': true,
-        'color-functional-notation': true
+        "lab-function": true,
+        "lch-function": true,
+        "oklab-function": true,
+        "oklch-function": true,
+        "color-functional-notation": true
       }
     }
   }
